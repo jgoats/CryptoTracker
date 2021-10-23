62a5fa3d58d72d388098b3a3f72a8780383c08aa
Justin Goats <justingoats@yahoo.com>
2021-10-22 22:09:44 -0700

    bar graph component used read data and is dynamic. Different crypto currencies can be used as will

--- src/components/bargraph/bargraph.js
+++ src/components/bargraph/bargraph.js
@@ -1,43 +1,57 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ReactDom from "react-dom";
 import "./bargraph.scss";
-
 import { Bar } from "react-chartjs-2";
-import moment from "moment";
 
-const labels = [];
-for (let i = 1; i <= 24; i++) {
-    labels.push(i);
-}
-const data = canvas => {
-    const ctx = canvas.getContext("2d");
-    const gradient = ctx.createLinearGradient(0, 0, 100, 0);
-    return {
-        backgroundColor: gradient,
-        labels,
-        datasets: [
-            {
-                data: [36004, 70000, 56000, 54000, 60000, 65000, 50000,
-                    36004, 70000, 56000, 54000, 60000, 65000, 50000,
-                    36004, 70000, 56000, 54000, 60000, 65000, 50000, 40000, 56000, 78000],
-                borderWidth: 3,
-                fill: true,
-                borderColor: "rgb(85,36,125)",
-                backgroundColor: "rgba(85,36,125,0.1)",
-            }
-        ]
-    };
-};
+
+
+
 export default class BarGraph extends React.Component {
+    componentDidMount() {
+        let customLabel = document.getElementsByClassName("custom-label-bar")[0];
+        let customLabels = [];
+        for (let i = 24; i > 0; i--) {
+            let label = React.createElement(
+                "div",
+                { key: i },
+                i
+            );
+            customLabels.push(label);
+        }
+        ReactDom.render(customLabels, customLabel);
+    }
     render() {
+        const labels = [];
+        for (let i = 1; i <= 24; i++) {
+            labels.push("");
+        }
+
+        const data = canvas => {
+            const ctx = canvas.getContext("2d");
+            const gradient = ctx.createLinearGradient(0, 0, 100, 0);
+            return {
+                backgroundColor: gradient,
+                labels,
+                datasets: [
+                    {
+                        data: this.props.totalvolumn,
+                        borderWidth: 2,
+                        fill: true,
+                        borderColor: "rgb(129,17,133)",
+                        backgroundColor: "rgba(129,17,133,0.8)",
+                    }
+                ]
+            };
+        };
         return (
             <div className="line-graph">
                 <Bar options={{
                     maintainAspectRatio: false,
                     plugins: {
                         legend: { display: false },
-                        tooltip: false
                     }
                 }} data={data} />
+                <div className="custom-label-bar"></div>
             </div>
         );
     }
