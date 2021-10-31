89183e627174b6e35d788d1d556624eca98b95ee
Justin Goats <justingoats@yahoo.com>
2021-10-30 21:32:58 -0700

    a component to send the user to the portfolio page when clicked. currently does not work

---
+++ src/components/portfolio/portfolio.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "./portfolio.scss";
+
+export default class ViewPortfolio extends Component {
+    render() {
+        return (
+            <div>
+                <button className="view-portfolio-btn-default">Portfolio</button>
+            </div>
+        )
+    }
+}
