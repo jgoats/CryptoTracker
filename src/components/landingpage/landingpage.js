a6916d22e610912b78a43bb1d27dbca7993944e2
Justin Goats <justingoats@yahoo.com>
2021-10-30 21:27:09 -0700

    added overview and coins components as well as props to send information to those controlled components

--- src/components/landingpage/landingpage.js
+++ src/components/landingpage/landingpage.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./landingpage.scss";
 import Navigation from "../navigation/navigation.js";
-import Favorites from "../favorites/favorites.js";
-import SearchBar from "../searchbar/searchbar.js";
+import Overview from "../overview/overview.js";
+import Coins from "../coins/coins.js";
 
 export default class Landingpage extends React.Component {
     constructor() {
@@ -14,7 +14,20 @@ export default class Landingpage extends React.Component {
     render() {
         return (
             <div className="landingpage-container">
-                <Navigation />
+                <Navigation currency={this.props.currency} currencyabbr={this.props.currencyabbr} changeCurrency={this.props.changeCurrency} />
+                <p className="overview-heading">{`${this.props.coin.replace(this.props.coin[0], this.props.coin[0].toUpperCase())} Overview`}</p>
+                <Overview
+                    handleSpinner={this.props.handleSpinner}
+                    coin={this.props.coin}
+                    totalvolumn={this.props.totalvolumn}
+                    graphColor={this.props.graphColor}
+                    graphlabels={this.props.graphlabels}
+                    handleDuration={this.props.handleDuration}
+                    price={this.props.price}
+                    currentcoinprice={this.props.currentcoinprice} />
+                <p className="overview-heading">Popular Coins</p>
+                <Coins wascurrencychanged={this.props.wascurrencychanged} setCurrency={this.props.setCurrency} currencyabbr={this.props.currencyabbr} />
+
             </div>
         )
     }
