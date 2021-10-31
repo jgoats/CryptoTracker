4b6133bde1bf66417270c002637398a9214d61ee
Justin Goats <justingoats@yahoo.com>
2021-10-30 21:35:24 -0700

    a component that will allow users to go to the coins page. Currently does not work

---
+++ src/components/viewcoins/viewcoins.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "./viewcoins.scss";
+
+export default class ViewCoins extends Component {
+    render() {
+        return (
+            <div>
+                <button className="view-coins-btn-default">Coins</button>
+            </div>
+        )
+    }
+}
