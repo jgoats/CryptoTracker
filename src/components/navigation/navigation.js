0c582b20b4b76c8615400c85def0547afc91267f
Justin Goats <justingoats@yahoo.com>
2021-10-30 21:30:23 -0700

    included coinheaderinfo component into the navigationto showcase dynamic data to the user in the future

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -4,18 +4,22 @@ import ChangeCurrency from "../changecurrency/changecurrency.js";
 import ViewCoins from "../viewcoins/viewcoins.js";
 import ViewPortfolio from "../portfolio/portfolio.js";
 import SearchCoins from "../searchcoins/searchcoins.js";
+import CoinHeaderInfo from "../coinheaderinfo/coinheaderinfo.js";
 
 
 export default class Navigation extends Component {
     render() {
         return (
-            <div className="navigation-container">
-                <section>
-                    <ViewCoins />
-                    <ViewPortfolio />
-                </section>
-                <SearchCoins />
-                <ChangeCurrency currency="USD" />
+            <div>
+                <div className="navigation-container">
+                    <section>
+                        <ViewCoins />
+                        <ViewPortfolio />
+                    </section>
+                    <SearchCoins />
+                    <ChangeCurrency changeCurrency={this.props.changeCurrency} currency={this.props.currency} currencyabbr={this.props.currencyabbr} />
+                </div>
+                <CoinHeaderInfo totalCoins={30} performance={20} mostCoin={"ETH"} secondMostCoin={"ETC"} thirdMostCoin={"DOGE"} />
             </div>
         )
     }
