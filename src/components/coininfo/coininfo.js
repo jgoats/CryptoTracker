dc78c44805bc4926b28a7252eb465b8d6600f671
Justin Goats <justingoats@yahoo.com>
2021-10-30 21:23:18 -0700

    included a componentDidUpdate() lifecycle method to dynamically update the coin data when a currency is changed

--- src/components/coininfo/coininfo.js
+++ src/components/coininfo/coininfo.js
@@ -9,13 +9,38 @@ export default class CoinInfo extends Component {
     constructor() {
         super();
         this.state = {
-            coins: []
+            coins: [],
+            setCurrency: ""
         }
     }
+    componentDidUpdate() {
+        if (this.props.currencyabbr !== this.state.setCurrency) {
+            let url = `api.coingecko.com/api/v3/coins/markets?vs_currency=${this.props.currencyabbr}&price_change_percentage=1h,24h,7d&sparkline=true&per_page=20`;
+            let newUrl = "https://" + url.replace(" ", "");
+            Axios({
+                method: "get",
+                url: newUrl,
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).then((result) => {
+                this.setState({
+                    coins: result.data
+                })
+            }).catch((err) => {
+                if (err) {
+                    console.log(err);
+                }
+            })
+            this.setState({
+                setCurrency: this.props.currencyabbr
+            })
+        }
+
+    }
     componentDidMount() {
-        let url = `api.coingecko.com/api/v3/coins/markets?vs_currency=${this.props.currency}&price_change_percentage=1h,24h,7d&sparkline=true&per_page=20`;
+        let url = `api.coingecko.com/api/v3/coins/markets?vs_currency=${this.props.currencyabbr}&price_change_percentage=1h,24h,7d&sparkline=true&per_page=20`;
         let newUrl = "https://" + url.replace(" ", "");
-        console.log(newUrl);
         Axios({
             method: "get",
             url: newUrl,
@@ -31,6 +56,9 @@ export default class CoinInfo extends Component {
                 console.log(err);
             }
         })
+        this.setState({
+            setCurrency: this.props.currencyabbr
+        })
     }
     render() {
         return (
@@ -39,16 +67,16 @@ export default class CoinInfo extends Component {
                     <p className="coin-number">{index + 1}</p>
                     <p className="coin-logo-container"><img className="coin-icon" src={coin.image} /></p>
                     <p className="coin-name">{coin.name}</p>
-                    <p>{coin.current_price.toPrecision(5)}</p>
+                    <p>{coin.current_price}</p>
                     {coin.price_change_percentage_1h_in_currency.toPrecision(3) > 0 ?
-                        <div style={{ display: "flex" }}><p style={{ color: "limegreen" }}>{coin.price_change_percentage_1h_in_currency.toPrecision(3)}</p><img style={{ width: "10px", marginLeft: "5px" }} src={ProfitIcon} /></div> :
-                        <div style={{ display: "flex" }}><p style={{ color: "#FF002B" }}>{coin.price_change_percentage_1h_in_currency.toPrecision(3)}</p><img style={{ width: "10px", marginLeft: "5px" }} src={LossIcon} /></div>}
+                        <div style={{ display: "flex" }}><p style={{ color: "limegreen" }}>{coin.price_change_percentage_1h_in_currency.toPrecision(3) + " %"}</p><img style={{ width: "10px", marginLeft: "5px" }} src={ProfitIcon} /></div> :
+                        <div style={{ display: "flex" }}><p style={{ color: "#FF002B" }}>{coin.price_change_percentage_1h_in_currency.toPrecision(3) + " %"}</p><img style={{ width: "10px", marginLeft: "5px" }} src={LossIcon} /></div>}
                     {coin.price_change_percentage_24h_in_currency.toPrecision(3) > 0 ?
-                        <div style={{ display: "flex" }}><p style={{ color: "limegreen" }}>{coin.price_change_percentage_24h_in_currency.toPrecision(3)}</p><img style={{ width: "10px", marginLeft: "5px" }} src={ProfitIcon} /></div> :
-                        <div style={{ display: "flex" }}><p style={{ color: "#FF002B", }}>{coin.price_change_percentage_24h_in_currency.toPrecision(3)}</p><img style={{ width: "10px", marginLeft: "5px" }} src={LossIcon} /></div>}
+                        <div style={{ display: "flex" }}><p style={{ color: "limegreen" }}>{coin.price_change_percentage_24h_in_currency.toPrecision(3) + " %"}</p><img style={{ width: "10px", marginLeft: "5px" }} src={ProfitIcon} /></div> :
+                        <div style={{ display: "flex" }}><p style={{ color: "#FF002B", }}>{coin.price_change_percentage_24h_in_currency.toPrecision(3) + " %"}</p><img style={{ width: "10px", marginLeft: "5px" }} src={LossIcon} /></div>}
                     {coin.price_change_percentage_7d_in_currency.toPrecision(3) > 0 ?
-                        <div style={{ display: "flex" }}><p style={{ color: "limegreen", }}>{coin.price_change_percentage_7d_in_currency.toPrecision(3)}</p><img style={{ width: "10px", marginLeft: "5px" }} src={ProfitIcon} /></div> :
-                        <div style={{ display: "flex" }}><p style={{ color: "#FF002B", }}>{coin.price_change_percentage_7d_in_currency.toPrecision(3)}</p><img style={{ width: "10px", marginLeft: "5px" }} src={LossIcon} /></div>}
+                        <div style={{ display: "flex" }}><p style={{ color: "limegreen", }}>{coin.price_change_percentage_7d_in_currency.toPrecision(3) + " %"}</p><img style={{ width: "10px", marginLeft: "5px" }} src={ProfitIcon} /></div> :
+                        <div style={{ display: "flex" }}><p style={{ color: "#FF002B", }}>{coin.price_change_percentage_7d_in_currency.toPrecision(3) + " %"}</p><img style={{ width: "10px", marginLeft: "5px" }} src={LossIcon} /></div>}
                     <LineGraphSmall price={coin.sparkline_in_7d.price}
                         color={coin.sparkline_in_7d.price[0] < coin.sparkline_in_7d.price[coin.sparkline_in_7d.price.length - 1] ?
                             "limegreen" : "#FF002B"} />
