a0480f89bf8b3ba15524eb12a13e588493f192f9
Justin Goats <justingoats@yahoo.com>
2021-11-16 22:04:55 -0800

    added a react-router-dom (link) to change the route on a button click

--- src/components/viewcoins/viewcoins.js
+++ src/components/viewcoins/viewcoins.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import "./viewcoins.scss";
+import { Link } from "react-router-dom";
 
 export default class ViewCoins extends Component {
     render() {
         return (
             <div>
-                <button className="view-coins-btn-default">Coins</button>
+                <Link to="/" className="view-coins-btn-default">Coins</Link>
             </div>
         )
     }
