6d43dbf9c1ff5c0ddb3de720a13a582e0b20c5b3
Justin Goats <justingoats@yahoo.com>
2021-11-16 22:03:04 -0800

    installed react-router-dom into dependancies

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     // webpack 5 comes with devServer which loads in development mode
     devServer: {
         port: 3000,
+
     },
     // Rules of how webpack will take our files, complie & bundle them for the browser 
     module: {
